d27b1acd57b7b3a676861b61656e648579980627
“Belen <beckiar23@gmail.com>
2023-03-20 15:24:26 -0400

    Prueba de retorno del numero 2

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -4,4 +4,7 @@ describe("Fizz buzz un numero", () => {
   it("deberia retornar 1", () => {
     expect(fizzbuzz(1)).toEqual(1);
   });
+  it("deberia retornar el numero 2", () => {
+    expect(fizzbuzz(2)).toEqual(2);
+  });
 });
